fc7b07b929d96061679c9d8361c42a7234c43597
soc <songyanhui>
2020-10-20 13:58:36 +0800

    快排

--- sort_algorithm/sort_algotithm.py
+++ sort_algorithm/sort_algotithm.py
@@ -51,6 +51,33 @@ def insert_sort(arr: list) -> list:
     return arr
 
 
+def quickly_sort(arr: list, left=None, right=None) -> list:
+    """快排"""
+    left = 0 if not isinstance(left, (int, float)) else left
+    right = len(arr) - 1 if not isinstance(right, (int, float)) else right
+    if left < right:
+        partition_index = partition(arr, left, right)
+        quickly_sort(arr, left, partition_index - 1)
+        quickly_sort(arr, partition_index + 1, right)
+    return arr
+
+
+def partition(arr: list, left: int, right: int) -> int:
+    pivot, index, i = left, left + 1, left + 1
+
+    while i <= right:
+        if arr[i] < arr[pivot]:
+            swap(arr, i, index)
+            index += 1
+        i += 1
+    swap(arr, pivot, index - 1)
+    return index - 1
+
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
 def shell_sort(arr: list) -> list:
     """
     希尔排序
